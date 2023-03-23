3b6293435bc54b17a3340835175e7ea409bde554
Juan David Rojas <juanrojas043@gmail.com>
2023-03-22 22:06:14 -0500

    Se cambio todo

--- proyecto5/azul/static/js/step.js
+++ proyecto5/azul/static/js/step.js
@@ -1,106 +1,175 @@
-const image0 = document.getElementById("imagen0");
-const image1 = document.getElementById("imagen1");
-const image2 = document.getElementById("imagen2");
-const image3 = document.getElementById("imagen3");
-const image4 = document.getElementById("imagen4");
-const image5 = document.getElementById("imagen5");
-const image6 = document.getElementById("imagen6");
-const image7 = document.getElementById("imagen7");
-const image8 = document.getElementById("imagen8");
-let isActive = true;
-let isActive2 = true;
+// const image0 = document.getElementById("imagen0");
+// const image1 = document.getElementById("imagen1");
+// const image2 = document.getElementById("imagen2");
+// const image3 = document.getElementById("imagen3");
+// const image4 = document.getElementById("imagen4");
+// const image5 = document.getElementById("imagen5");
+// const image6 = document.getElementById("imagen6");
+// const image7 = document.getElementById("imagen7");
+// const image8 = document.getElementById("imagen8");
+// let isActive = true;
+// let isActive2 = true;
+// console.log("hola");
+// const power = () => {
+//   image0.style.display = "none";
+//   image1.style.display = "block";
+//   setTimeout(() => {
+//     image1.style.display = "none";
+//     image2.style.display = "block";
+//   }, 3000);
 
-const power = () => {
-  image0.style.display = "none";
-  image1.style.display = "block";
-  setTimeout(() => {
-    image1.style.display = "none";
-    image2.style.display = "block";
-  }, 3000);
-  // 3000
+//   setTimeout(() => {
+//     image2.style.display = "none";
+//     image3.style.display = "block";
+//     cambiaClase();
+//   }, 5000);
+// };
 
-  setTimeout(() => {
-    image2.style.display = "none";
-    image3.style.display = "block";
-    cambiaClase();
-  }, 5000);
-  // 5000
+// const showImagez = () => {
+//   if (isActive == true) {
+//     image4.style.display = "block";
+//     isActive = false;
+//   } else if (isActive == false) {
+//     image4.style.display = "none";
+//     isActive = true;
+//   }
+// };
+
+// const showImage2z = () => {
+//   if (isActive2 == true) {
+//     image5.style.display = "block";
+//     isActive2 = false;
+//   } else if (isActive2 == false) {
+//     image5.style.display = "none";
+//     isActive2 = true;
+//   }
+// };
+
+// const ejecutar = () => {
+//   image4.style.display = "none";
+//   image6.src = "/media/img/6.gif";
+//   image6.style.display = "block";
+
+//   function desvanecer() {
+//     setTimeout(() => {
+//       image6.style.display = "none";
+//       image3.style.display = "none";
+//       image7.style.display = "block";
+//     }, 6000);
+//   }
+
+//   function desvanecer2() {
+//     setTimeout(() => {
+//       image8.style.display = "block";
+//     }, 7000);
+//   }
+
+//   desvanecer();
+//   desvanecer2();
+// };
+
+// const cancelar = () => {
+//   image4.style.display = "none";
+// };
+
+// const apagar = () => {
+//   image3.style.display = "none";
+//   image5.style.display = "none";
+//   image0.style.display = "block";
+// };
+
+// const reset = () => {
+//   image3.style.display = "none";
+//   image5.style.display = "none";
+//   image0.style.display = "none";
+//   power();
+// };
+
+
+const main1 = document.getElementById("main1");
+const main2 = document.getElementById("main2");
+const main3 = document.getElementById("main3");
+const main4 = document.getElementById("main4");
+const img9 = document.getElementById("img-9");
+const papelera = document.getElementById("papelera");
+const img10 = document.getElementById("img-10");
+let bandera = true;
+let bandera2 = true;
+
+const showImage1 = () => {
+  main1.style.display = "none";
+  main2.style.display = "block";
 };
-
-const showImage = () => {
-  //image3.style.display = "relative";
- // image4.style.position = "absolute";
-  if (isActive == true) {
-    image4.style.display = "block";
-    isActive = false;
-  } else if (isActive == false) {
-    image4.style.display = "none";
-    isActive = true;
-  }
-};
-
 const showImage2 = () => {
-  //image3.style.display = "relative";
-  //image5.style.position = "absolute";
-  if (isActive2 == true) {
-    image5.style.display = "block";
-    isActive2 = false;
-  } else if (isActive2 == false) {
-    image5.style.display = "none";
-    isActive2 = true;
+  main2.style.display = "none";
+  main3.style.display = "block";
+};
+const showImage3 = () => {
+  main3.style.display = "none";
+  main4.style.display = "block";
+};
+const showImage4 = () => {
+  if (bandera == true) {
+    img10.style.display = "block";
+    bandera = false;
+  } else if (bandera == false) {
+    img10.style.display = "none";
+    bandera = true;
   }
 };
 
-const ejecutar = () => {
-  //image3.style.display = "relative";
-  image4.style.display = "none";
-  image6.src = "/media/img/6.gif";
-  image6.style.display = "block";
-  //image6.style.position = "absolute";
-  // image6.style.top = "25%";
-  // image6.style.left = "20%";
+function actualizarHora() {
+  const fechaActual = new Date();
+  document.getElementById("fecha").innerHTML = fechaActual.toLocaleString();
+}
 
-  function desvanecer() {
-    setTimeout(() => {
-      image6.style.display = "none";
-      image3.style.display = "none";
-      image7.style.display = "block";
-    }, 6000);
-    // 6000
+actualizarHora();
+setInterval(actualizarHora, 1000);
+
+img9.onmouseover = function () {
+  img9.classList.remove("img-9");
+  img9.classList.add("img-9-cambiado");
+};
+
+img9.onmouseout = function () {
+  img9.classList.remove("img-9-cambiado");
+  img9.classList.add("img-9");
+};
+
+papelera.onmouseover = function () {
+  papelera.classList.remove("papelera");
+  papelera.classList.add("papelera-cambiado");
+};
+
+papelera.onmouseout = function () {
+  papelera.classList.remove("papelera-cambiado");
+  papelera.classList.add("papelera");
+};
+const img11 = document.getElementById("img-11");
+const c1Img11 = document.getElementById("cont2-img-11");
+const c2Img11 = document.getElementById("cont3-img-11");
+
+const showImage5 = () => {
+  if (bandera2 == true) {
+    img11.style.display = "block";
+    c1Img11.classList.add("cont2-img-11");
+    c2Img11.classList.add("cont3-img-11");
+    bandera2 = false;
+  } else if (bandera2 == false) {
+    img11.style.display = "none";
+    c1Img11.classList.remove("cont2-img-11");
+    c2Img11.classList.remove("cont3-img-11");
+    bandera2 = true;
   }
-
-  function desvanecer2() {
-    setTimeout(() => {
-      //image7.style.display = "relative";
-      image8.style.display = "block";
-      //image8.style.position = "absolute";
-      //image8.style.top = "22%";
-      //image8.style.left = "30%";
-    }, 7000);
-    // 7000
-  }
-  
-  desvanecer();
-  desvanecer2();
 };
-
-const cancelar = () => {
-  image4.style.display = "none";
-};
-
-const apagar = () => {
-  image3.style.display = "none";
-  image5.style.display = "none";
-  image0.style.display = "block";
-};
-
-const reset = () => {
-  image3.style.display = "none";
-  image5.style.display = "none";
-  image0.style.display = "none";
-  power();
-};
-
+// Apaga Máquina
+const showImage6 = () => {
+  alert("Apagar")
+}
+// Reinicia Máquina
+const showImage7 = () => {
+  alert("Reiniciar")
+}
 
 
 // let mostrarImagenClic = () => {
