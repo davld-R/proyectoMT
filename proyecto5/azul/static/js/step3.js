0b208722e9b07e79550da7d92b629a50d70ce0d7
Juan David Rojas <juanrojas043@gmail.com>
2023-04-15 23:17:39 -0500

    Se cambio en la parte de step1 la parte del html

--- proyecto5/azul/static/js/step3.js
+++ proyecto5/azul/static/js/step3.js
@@ -1,39 +1,29 @@
 const audio = new Audio("/media/img/mouse.mp3");
 const contenedorOpciones = document.getElementById("contenedorOpciones");
-const difuminarPC = document.getElementById("difuminarPC");
 let semaforo1 = true;
-const contenedorDifuminar = document.getElementById("contenedorDifuminar");
-const contenedorCPU = document.getElementById("contenedorCPU");
+const imgMonitor = document.getElementById("imgFondoPrincipal");
+const imgFondo = document.getElementById("imgFondo");
 
-const mostrarCPU = () => {
+function mostrarOpciones() {
   audio.play();
   if (semaforo1 == true) {
     contenedorOpciones.style.display = "flex";
+    imgFondo.style.display = "block";
     semaforo1 = false;
   } else if (semaforo1 == false) {
     contenedorOpciones.style.display = "none";
+    imgFondo.style.display = "none";
     semaforo1 = true;
   }
-};
+}
 
-contenedorCPU.onmouseover = function () {
-  contenedorCPU.classList.remove("contenedorCPU");
-  contenedorCPU.classList.add("contenedorCPUCambio");
-};
-contenedorCPU.onmouseout = function () {
-  contenedorCPU.classList.remove("contenedorCPUCambio");
-  contenedorCPU.classList.add("contenedorCPU");
-};
-
-const mostrarEYE = () => {
-  audio.play();
-  contenedorDifuminar.style.display = "block";
-};
-const cerrarDifuminar = () => {
-  audio.play();
-  contenedorDifuminar.style.display = "none";
-};
+imgMonitor.onclick = mostrarOpciones;
+imgFondo.onclick = mostrarOpciones;
 
 document.getElementById("contenedorBotonIR").onclick = function () {
   audio.play();
+  location.href = "{% url 'ejecucionPC' %}";
 };
+
+
+
